6d386dcfb08fc123ab67ebc0404588ed83111e87
Kyle Dong <dong.ky@northeastern.edu>
2025-03-19 23:37:27 -0700

    The start

--- src/main/kotlin/io/github/techtastic/vs_addon_template/VSAddonTemplateMod.kt
+++ src/main/kotlin/io/github/techtastic/vs_addon_template/VSAddonTemplateMod.kt
@@ -1,14 +1,25 @@
-package io.github.techtastic.vs_addon_template
+package io.github.techtastic.NBTtoVS
 
 import net.minecraftforge.fml.common.Mod
+import net.minecraft.entity.Entity
+import net.minecraft.entity.LivingEntity
+import net.minecraft.nbt.CompoundNBT
+import net.minecraftforge.event.entity.EntityJoinWorldEvent
+import net.minecraftforge.eventbus.api.IEventBus
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.common.Mod
 
-@Mod(VSAddonTemplateMod.MOD_ID)
-class VSAddonTemplateMod {
+@Mod(NBTtoVS.MOD_ID)
+class NBTtoVS {
     init {
         // Common Code Here
     }
 
     companion object {
-        const val MOD_ID = "vs_addon_template"
+        const val MOD_ID = "NBTtoVS"
+    }
+
+    fun readNBT(entity: Entity): String {
+
     }
 }
