95a49ac79b37ff15539a480fdcaeee93f32694f1
Kyle Dong <dong.ky@northeastern.edu>
2025-03-20 21:42:02 -0700

    Implemented half the bridge. Can grab NBT data with Thrust as a tag

--- src/main/kotlin/io/github/techtastic/vs_addon_template/VSAddonTemplateMod.kt
+++ src/main/kotlin/io/github/techtastic/vs_addon_template/VSAddonTemplateMod.kt
@@ -1,24 +1,44 @@
 package io.github.techtastic.NBTtoVS
 
 import net.minecraftforge.fml.common.Mod
-import net.minecraft.world.entity.Entity
-import net.minecraft.world.entity.LivingEntity
-import net.minecraft.nbt.CompoundTag
-import net.minecraftforge.event.entity.EntityJoinLevelEvent
-import net.minecraftforge.eventbus.api.IEventBus
-import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.server.level.ServerPlayer
 
 @Mod(NBTtoVS.MOD_ID)
 class NBTtoVS {
     init {
-        // Common Code Here
+        println("The Mod Works Tim")
     }
 
     companion object {
         const val MOD_ID = "nbt_to_vs"
     }
 
-    fun readNBT(entity: Entity): String {
+    fun isBlockEntity(world: Level, pos: BlockPos): Boolean{
+        val blockEntity: BlockEntity? = world.getBlockEntity(pos)
+        return blockEntity != null
+    }
 
+    fun containsNBTtagThrust(world: Level, pos: BlockPos): Boolean {
+        if (isBlockEntity(world, pos)) {
+            return getBlockEntityNBT(world, pos).contains("Thrust", ignoreCase = true)
+        }
+        return false;
+    }
+
+    fun getBlockEntityNBT(world: Level, pos: BlockPos): String {
+        val blockEntity = world.getBlockEntity(pos)
+
+        if (blockEntity != null) {
+            // Saves block entity NBT to 'nbt' as a compound tag
+            val nbt = blockEntity.saveWithFullMetadata()
+
+            // Return the NBT data as a string
+            return nbt.toString()
+        } else {
+            return "No block entity found at position $pos."
+        }
     }
 }
